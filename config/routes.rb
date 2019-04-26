6401567ee80b7a1a5c71a5bc155dca6446bf2f07
github username <github email address>
2019-04-26 03:30:01 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
+  resources :appointments, :only => :show
 
   get '/doctor:id', to: 'doctors#show'
   get '/patient:id', to: 'patients#show'
