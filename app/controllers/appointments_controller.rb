94475f11bea8cd1114db0c1b9e804b6907eda829
AudreyJones <audrey.eliz.jones@gmail.com>
2019-05-02 23:42:36 -0400

    Done.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,8 @@
+require 'pry'
 class AppointmentsController < ApplicationController
+    def show
+
+        @appointment = Appointment.find_by_id(params[:id])
+        # binding.pry
+    end
 end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,5 @@
 class PatientsController < ApplicationController
+    def show
+
+    end
 end
